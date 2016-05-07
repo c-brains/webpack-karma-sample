78877286ea9e8a0917162bba3a03fade66c3e8ec
Koji Tanaka <tenkoma@gmail.com>
2016-05-08 01:36:48 +0900

    initial commit

---
+++ src/calc-util.js
@@ -0,0 +1,5 @@
+module.exports = {
+    add : function add(value1, value2) {
+        return value1 + value2;
+    }
+}
---
+++ test/calc-util-spec.js
@@ -0,0 +1,12 @@
+
+var calcUtil = require('../src/calc-util.js');
+var assert = require('power-assert');
+
+describe('add関数のテスト', function() {
+    it('1+2は3である', function() {
+        assert(calcUtil.add(1, 2) === 3);
+    });
+    it('1+2は4である', function() {
+        assert(calcUtil.add(1, 2) === 4);
+    });
+})
