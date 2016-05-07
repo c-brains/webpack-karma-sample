2696ceeda5f0bef679020454e1bb32884bfb3aac
Koji Tanaka <tenkoma@gmail.com>
2016-05-08 03:58:35 +0900

    rewrite test file by es2015 style

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,12 +1,12 @@
 
-var calcUtil = require('../src/calc-util.js');
-var assert = require('power-assert');
+import calcUtil from '../src/calc-util.js'
+import assert from 'power-assert'
 
-describe('add関数のテスト', function() {
-    it('1+2は3である', function() {
-        assert(calcUtil.add(1, 2) === 3);
-    });
-    it('1+2は4である', function() {
-        assert(calcUtil.add(1, 2) === 4);
-    });
-});
+describe('add関数のテスト', () => {
+    it('1+2は3である', () => {
+        assert(calcUtil.add(1, 2) === 3)
+    })
+    it('1+2は4である', () => {
+        assert(calcUtil.add(1, 2) === 4)
+    })
+})
